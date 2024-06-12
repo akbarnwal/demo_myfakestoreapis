62dcb7725afa0b6ee50d22be2f94a36e3a3102c1
Bitrise Buildbot <bitrise-admins@backbase.com>
2024-06-12 08:39:53 +0530

    Added addManyProducts() in the test class

--- src/test/java/in/anilbarnwal/demo_myfakestoreapis/DemoMyfakestoreapisApplicationTests.java
+++ src/test/java/in/anilbarnwal/demo_myfakestoreapis/DemoMyfakestoreapisApplicationTests.java
@@ -115,4 +115,36 @@ class DemoMyfakestoreapisApplicationTests {
             }
         }
     }
+
+//    @Test
+//    @Transactional
+//    void saveDummyProductsList(){
+//        List<Product> products = productRepository.findAll();
+//        for (Product product : products) {
+//            productRepository.save(product);
+//        }
+//    }
+
+    @Test
+    void addManyProducts() {
+        double basePrice = 2000;
+        String productName = "toy";
+        String productDescription = "toy number";
+        String imageUrl = "www.imgur.com/";
+        String categoryName = "toys";
+
+        for (int i = 1; i <= 100; i++) {
+            Product product = new Product();
+            product.setTitle(productName + "-" + i);
+            product.setDescription( productDescription + " # " + i);
+            product.setPrice("" +basePrice + i);
+            Category category = new Category();
+            category.setTitle(categoryName);
+            product.setCategory(category);
+            product.setImage(imageUrl + i);
+            productRepository.save(product);
+        }
+
+
+    }
 }
